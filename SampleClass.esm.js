3011990e75b1276324e83c23040c623eaaf78e2d
DataGridXL <info@datagridxl.com>
2023-06-16 18:43:47 +0000

    Update from https://github.com/DataGridXL/sampleproject/commit/694e045559e0eb7ea132d9eeacf8a5139ae1bbc6

---
+++ SampleClass.esm.js
@@ -0,0 +1,7 @@
+/**
+ * SampleClass
+ * v0.23.06.16
+ * (c) Vanilla Components
+ */
+
+export default class SampleClass{constructor(){}someMethod(){document.body.innerHTML="nee toch!"}someMethod2(){document.body.textContent="nee toch!"}}
